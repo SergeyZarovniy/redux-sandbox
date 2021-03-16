c7909fa3abf6342b23cdda7555c680690a98f061
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-03-16 16:53:05 +0100

    Create Reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import {createStore} from 'redux';
+
+
+
+const reducer = ( state = 0, action ) => {
+
+ switch (action.type){
+     case "INC":
+         return state + 1;
+     default :
+         return state;    
+ }
+};
+
+
+const store = createStore(reducer);
+
+store.subscribe( () => {
+console.log(store.getState())
+});
+
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
