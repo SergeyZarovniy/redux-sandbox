6443f9bb9c070a9ee48818794dacb7b00c628be4
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-03-17 19:09:38 +0100

    Working with reducer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const reducer = ( state = 0, action ) => {
          return state + 1;
      case "DEC":
          return state - 1;
+     case "RND":
+         return state + action.payload;
      default :
          return state;    
  }
@@ -27,6 +29,14 @@ document
   .getElementById('dec')
   .addEventListener('click', () => {
       store.dispatch({type:"DEC"})
+  });
+
+  document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+      const payload = Math.floor(Math.random()*10);
+      store.dispatch({type:"RND",
+      payload})
   });
 
   const update = () => {
